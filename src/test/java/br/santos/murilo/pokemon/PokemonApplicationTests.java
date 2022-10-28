e8396c8e2c9e99429eba76f09376d7a27a3b7fc2
Murilo Santos <mucrilobass@gmail.com>
2022-10-28 12:42:32 -0400

    Iniciando

---
+++ src/test/java/br/santos/murilo/pokemon/PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.santos.murilo.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
